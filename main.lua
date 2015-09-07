05933691f3e62556bc7614f24ffff5f95f63282f
ohjustjames <ohjustjames@gmail.com>
2015-09-07 20:41:42 +0100

    It's annoying when vim makes temp files

--- main.lua
+++ main.lua
@@ -3,5 +3,5 @@ function love.load()
 end
 
 function love.draw()
-	love.graphics.printf("hello", 0, love.graphics.getHeight() / 2, love.graphics.getWidth(), "center")
+	love.graphics.printf("hello", 0, love.graphics.getWidth() / 2, love.graphics.getWidth(), "center")
 end
