6e2165af636655acbb752550656d7b95ae95cdd2
ohjustjames <ohjustjames@gmail.com>
2015-09-08 20:20:09 +0100

    Added a license file, added TLBind and middleclass and made the hello text actually centered whilst on-screen.

--- main.lua
+++ main.lua
@@ -1,7 +1,12 @@
-function love.load()
+Class = require "middleclass" -- Include the Middleclass system
 
+function love.load()
 end
 
 function love.draw()
-	love.graphics.printf("hello", 0, love.graphics.getWidth() / 2, love.graphics.getWidth(), "center")
+	love.graphics.printf("hello", 0, love.graphics.getHeight() / 2, love.graphics.getWidth(), "center")
+end
+
+function love.update(dt)
+
 end
