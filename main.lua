8d72f40d368fb33294c1f307ba2291772c66914d
ohjustjames <ohjustjames@gmail.com>
2015-09-11 19:12:58 +0100

    Added a button class

--- main.lua
+++ main.lua
@@ -1,12 +1,15 @@
 Class = require "libraries.middleclass" -- Include the Middleclass system
+Room = 0
 
 function love.load()
 	-- Load the classes
 	require "classes.base"
+	require "classes.button"
+	o = Button(0, 10, 10, "hello", function() print "hi" end)
 end
 
 function love.draw()
-	love.graphics.printf("hello", 0, love.graphics.getHeight() / 2, love.graphics.getWidth(), "center")
+	o:draw()
 end
 
 function love.update(dt)
